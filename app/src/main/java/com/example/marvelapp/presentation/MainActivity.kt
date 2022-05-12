015487a4f36d7881f5212aed377260b9cd62d8e6
Pedro Scott <pedroscott.dev@gmail.com>
2022-05-12 00:23:41 -0300

    M02: Setup base navigation from project and Update base fragment and activity

--- app/src/main/java/com/example/marvelapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/marvelapp/presentation/MainActivity.kt
@@ -1,8 +1,46 @@
 package com.example.marvelapp.presentation
 
+import android.os.Bundle
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.example.marvelapp.R
 import com.example.marvelapp.databinding.ActivityMainBinding
 import com.example.marvelapp.presentation.base.BaseActivity
 
 class MainActivity : BaseActivity<ActivityMainBinding>(
     inflater = ActivityMainBinding::inflate
-)
+) {
+    private val appBarConfiguration: AppBarConfiguration by lazy {
+        AppBarConfiguration(
+            setOf(
+                R.id.heroesFragment,
+                R.id.favoritesFragment,
+                R.id.aboutFragment,
+            )
+        )
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setView()
+        setNavigationListener()
+    }
+
+    private fun setView() = binding.run {
+        mainBottomNav.setupWithNavController(findNavController(R.id.mainNavHost))
+        mainTopAppBar.setupWithNavController(
+            findNavController(R.id.mainNavHost),
+            appBarConfiguration
+        )
+    }
+
+    private fun setNavigationListener() {
+        findNavController(R.id.mainNavHost).addOnDestinationChangedListener { _, destination, _ ->
+            appBarConfiguration.topLevelDestinations.contains(destination.id).let { isTopLevelDestination ->
+                if (!isTopLevelDestination)
+                    binding.mainTopAppBar.setNavigationIcon(R.drawable.ic_back)
+            }
+        }
+    }
+}
